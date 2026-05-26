284391c74dd1e4bdb9d042c26e090282b6fb65c1
刘应铎 <5416367+liu_ying_duo@user.noreply.gitee.com>
2026-05-26 14:16:17 +0800

    修改服务

--- deploy.sh
+++ deploy.sh
@@ -113,7 +113,7 @@ case "$CMD" in
     stop)    do_stop;   green "已停止。" ;;
     status)  do_status ;;
     logs)    do_logs ;;
-    restart) do_stop; do_start ;;
+    restart) do_stop; do_build; do_start ;;
     deploy)  do_build; do_start ;;
     *)
         echo "未知命令: $CMD"
